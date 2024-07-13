fecbdd29d996fab128dfce8fb3d8dcda5a8d4345
dainara Luchina <rodrigolucchina@hotmail.com>
2024-07-13 13:54:07 -0300

    Rook moves implementation

---
+++ src/main/java/chess/ChessException.java
@@ -0,0 +1,17 @@
+package chess;
+
+import boardgame.BoardException;
+
+import java.io.Serial;
+
+public class ChessException extends BoardException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ChessException(String message) {
+        super(message);
+    }
+
+
+
+}
--- src/main/java/org/example/UI.java
+++ src/main/java/org/example/UI.java
@@ -1,7 +1,24 @@
 package org.example;
 
 import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class UI {
+
+    public static ChessPosition readChessPosition(Scanner sc) {
+        try {
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        }
+        catch (RuntimeException error) {
+            throw new InputMismatchException("Error reading user input");
+        }
+    }
 
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i=0; i < pieces.length; i++) {
